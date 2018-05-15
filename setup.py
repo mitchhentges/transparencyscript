1c75be20b24f70332ca79f48cc463a3ccd649ba6
Aki Sasaki <aki@escapewindow.com>
2018-05-15 12:30:10 -0700

    add .travis.yml, py37 testing; requirements.txt

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     classifiers=(
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ),
     packages=find_packages(),
     install_requires=[
